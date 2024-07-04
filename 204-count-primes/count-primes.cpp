2a6755e3cba2ef3840b2ace48fa4b7abf2420bc1
Harsh Gambhir <harshgambhir48@gmail.com>
2024-07-05 00:35:14 +0530

    Time: 525 ms (17.93%) | Memory: 11.4 MB (92.29%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -8,7 +8,7 @@ public:
         for(int i = 2; i<n; i++){
             if(prime[i]){
                 count++;
-                for(int j = i; j<n; j=j+i){
+                for(int j = i*2; j<n; j=j+i){
                     prime[j] = false;
                 }
             }
